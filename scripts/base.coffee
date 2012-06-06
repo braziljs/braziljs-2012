0f15518d0ee21e03f9d23a7c74a92ec60245582b
Ricardo Tomasi <ricardobeat@gmail.com>
2012-06-06 20:59:17 -0300

    Palestrantes na home

--- scripts/base.coffee
+++ scripts/base.coffee
@@ -1,6 +1,128 @@
+$ = (sel) -> Array::slice.call document.querySelectorAll sel
+$$ = (sel) -> document.querySelector sel
+
 poa = document.getElementById 'poa'
 
 window.addEventListener 'scroll', (e) ->
     position = document.body.scrollTop
     poa.style.backgroundPositionY = (-position/2.2).toFixed(1) + 'px' if position < 600
     return
+
+$('.speaker-image').forEach (svg) ->
+    return
+
+    pattern = svg.getElementsByTagName('image')[0]
+    href = pattern.getAttribute('xlink:href')
+    active = href.replace '.jpg', '-active.jpg'
+
+    svg.addEventListener 'mouseover', ->
+        pattern.setAttribute 'xlink:href', active
+
+    svg.addEventListener 'mouseout', ->
+        pattern.setAttribute 'xlink:href', href
+
+###
+Faz buracos nas fotos dos palestrantes. PNG é para os fracos,
+e assim as imagens ficam muito mais leves
+###
+createCanvas = (w, h) ->
+    canvas = document.createElement 'canvas'
+    canvas.width = w
+    canvas.height = h
+    return canvas
+
+circle = (ctx, x, y, r) ->
+    ctx.beginPath()
+    ctx.arc(x, y, r, 0, Math.PI*2, true)
+    ctx.closePath()
+
+stamp = (image, x, y) ->
+    canvas = createCanvas 120, 120
+    ctx = canvas.getContext '2d'
+    ctx.fillStyle = ctx.createPattern image, 'no-repeat'
+    ctx.lineWidth = 0.5
+    ctx.strokeStyle = 'rgba(100,100,100,0.2)'
+    circle ctx, 60, 60, 60
+    ctx.translate x, y
+    ctx.fill()
+    ctx.stroke()
+    return canvas
+    
+
+pokeHoles = (image) ->
+    # normal
+    canvas = stamp image, 0, 0
+    image.parentNode.appendChild canvas
+    # hover
+    canvas = stamp image, 0, -120
+    canvas.className = 'active'
+    image.parentNode.appendChild canvas
+    # remover imagem original
+    image.parentNode.removeChild image
+
+$('.speakers .photo img').forEach (image) ->
+    img = new Image
+    img.onload = -> pokeHoles image
+    img.src = image.src
+
+###
+Expandir mais info sobre palestrante
+###
+
+cssInfo = document.createElement 'style'
+cssInfo.type = 'text/css'
+$$('head').appendChild cssInfo
+
+rules = ""
+
+$('.speakers p').forEach (p, i) ->
+
+    more = document.createElement 'button'
+    more.className = 'more'
+    more.innerHTML = '+'
+    p.parentNode.insertBefore more, p
+
+    more.parentNode.addEventListener 'click', (e) ->
+        e.preventDefault()
+        if p.className.indexOf('open') < 0
+            p.className = 'open'
+        else
+            p.className = ''
+
+    # medir altura do texto e criar regra para transition
+    height = parseInt(getComputedStyle(p).height, 10)
+    rules += "#info-#{i}.open { height:#{height}px; }"
+    p.id = "info-#{i}"
+
+if cssInfo.styleSheet
+    cssInfo.styleSheet.cssText = rules
+else
+    cssInfo.appendChild document.createTextNode rules
+
+offsetTop = (el) ->
+    return unless el.offsetParent
+    top = 0
+    loop
+        top += el.offsetTop
+        break if not el = el.offsetParent
+    return top
+
+###
+Scrolling effect for navigation
+###
+if screen.width > 600
+    $('#nav a').forEach (a) ->
+        a.addEventListener 'click', (e) ->
+            target = e.target.hash
+            return unless target
+            end = offsetTop $$(target)
+            pos = 0
+            start = document.body.scrollTop
+            range = end - start
+            direction = if end > start then 1 else -1
+            move = ->
+                pos += direction * 5
+                document.body.scrollTop = start + range * Math.sin(Math.PI/2 * pos/100)
+                webkitRequestAnimationFrame(move) unless pos > 99
+            webkitRequestAnimationFrame move
+
--- scripts/base.js
+++ scripts/base.js
@@ -1,5 +1,13 @@
 (function() {
-  var poa;
+  var $, $$, circle, createCanvas, cssInfo, offsetTop, poa, pokeHoles, rules, stamp;
+
+  $ = function(sel) {
+    return Array.prototype.slice.call(document.querySelectorAll(sel));
+  };
+
+  $$ = function(sel) {
+    return document.querySelector(sel);
+  };
 
   poa = document.getElementById('poa');
 
@@ -10,5 +18,144 @@
       poa.style.backgroundPositionY = (-position / 2.2).toFixed(1) + 'px';
     }
   });
+
+  $('.speaker-image').forEach(function(svg) {
+    var active, href, pattern;
+    return;
+    pattern = svg.getElementsByTagName('image')[0];
+    href = pattern.getAttribute('xlink:href');
+    active = href.replace('.jpg', '-active.jpg');
+    svg.addEventListener('mouseover', function() {
+      return pattern.setAttribute('xlink:href', active);
+    });
+    return svg.addEventListener('mouseout', function() {
+      return pattern.setAttribute('xlink:href', href);
+    });
+  });
+
+  /*
+  Faz buracos nas fotos dos palestrantes. PNG é para os fracos,
+  e assim as imagens ficam muito mais leves
+  */
+
+  createCanvas = function(w, h) {
+    var canvas;
+    canvas = document.createElement('canvas');
+    canvas.width = w;
+    canvas.height = h;
+    return canvas;
+  };
+
+  circle = function(ctx, x, y, r) {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, Math.PI * 2, true);
+    return ctx.closePath();
+  };
+
+  stamp = function(image, x, y) {
+    var canvas, ctx;
+    canvas = createCanvas(120, 120);
+    ctx = canvas.getContext('2d');
+    ctx.fillStyle = ctx.createPattern(image, 'no-repeat');
+    ctx.lineWidth = 0.5;
+    ctx.strokeStyle = 'rgba(100,100,100,0.2)';
+    circle(ctx, 60, 60, 60);
+    ctx.translate(x, y);
+    ctx.fill();
+    ctx.stroke();
+    return canvas;
+  };
+
+  pokeHoles = function(image) {
+    var canvas;
+    canvas = stamp(image, 0, 0);
+    image.parentNode.appendChild(canvas);
+    canvas = stamp(image, 0, -120);
+    canvas.className = 'active';
+    image.parentNode.appendChild(canvas);
+    return image.parentNode.removeChild(image);
+  };
+
+  $('.speakers .photo img').forEach(function(image) {
+    var img;
+    img = new Image;
+    img.onload = function() {
+      return pokeHoles(image);
+    };
+    return img.src = image.src;
+  });
+
+  /*
+  Expandir mais info sobre palestrante
+  */
+
+  cssInfo = document.createElement('style');
+
+  cssInfo.type = 'text/css';
+
+  $$('head').appendChild(cssInfo);
+
+  rules = "";
+
+  $('.speakers p').forEach(function(p, i) {
+    var height, more;
+    more = document.createElement('button');
+    more.className = 'more';
+    more.innerHTML = '+';
+    p.parentNode.insertBefore(more, p);
+    more.parentNode.addEventListener('click', function(e) {
+      e.preventDefault();
+      if (p.className.indexOf('open') < 0) {
+        return p.className = 'open';
+      } else {
+        return p.className = '';
+      }
+    });
+    height = parseInt(getComputedStyle(p).height, 10);
+    rules += "#info-" + i + ".open { height:" + height + "px; }";
+    return p.id = "info-" + i;
+  });
+
+  if (cssInfo.styleSheet) {
+    cssInfo.styleSheet.cssText = rules;
+  } else {
+    cssInfo.appendChild(document.createTextNode(rules));
+  }
+
+  offsetTop = function(el) {
+    var top;
+    if (!el.offsetParent) return;
+    top = 0;
+    while (true) {
+      top += el.offsetTop;
+      if (!(el = el.offsetParent)) break;
+    }
+    return top;
+  };
+
+  /*
+  Scrolling effect for navigation
+  */
+
+  if (screen.width > 600) {
+    $('#nav a').forEach(function(a) {
+      return a.addEventListener('click', function(e) {
+        var direction, end, move, pos, range, start, target;
+        target = e.target.hash;
+        if (!target) return;
+        end = offsetTop($$(target));
+        pos = 0;
+        start = document.body.scrollTop;
+        range = end - start;
+        direction = end > start ? 1 : -1;
+        move = function() {
+          pos += direction * 5;
+          document.body.scrollTop = start + range * Math.sin(Math.PI / 2 * pos / 100);
+          if (!(pos > 99)) return webkitRequestAnimationFrame(move);
+        };
+        return webkitRequestAnimationFrame(move);
+      });
+    });
+  }
 
 }).call(this);
